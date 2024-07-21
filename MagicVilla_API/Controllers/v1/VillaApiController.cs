49b0d3299c5fa20ddb0b9046dcc3e2ded6f6e9fe
Rahman Syeedur <rahman.syeedur@valmetpartners.com>
2024-07-20 19:32:34 -0500

    Added code for data filter and search functionality

--- MagicVilla_API/Controllers/v1/VillaApiController.cs
+++ MagicVilla_API/Controllers/v1/VillaApiController.cs
@@ -37,12 +37,21 @@ namespace MagicVilla_API.Controllers.v1
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
-        public async Task<ActionResult<APIResponse>> GetVillas()
+        public async Task<ActionResult<APIResponse>> GetVillas([FromQuery(Name = "filterOccupancy")] int? occupancy, [FromQuery] string? search)
         {
             try
             {
                 //_logger.LogInformation("Get all villas");
-                response.Result = await _dbVilla.GetAllAsync();
+                IEnumerable<Villa> villaList;
+                if (occupancy > 0) {
+                    villaList = await _dbVilla.GetAllAsync(x=>x.Occupancy == occupancy);
+                } else {
+                    villaList = await _dbVilla.GetAllAsync();
+                }
+                if (!string.IsNullOrEmpty(search)) {
+                    villaList = villaList.Where(x => x.Name.ToLower().Contains(search));
+                }
+                response.Result = _mapper.Map<List<VillaDTO>>(villaList);
                 response.StatusCode = HttpStatusCode.OK;
                 return Ok(response);
             }
